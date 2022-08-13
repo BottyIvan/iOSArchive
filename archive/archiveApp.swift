8b0394533e61ff91b2c5f2cd7285a7a81b97b675
thaG33k <ivanbotty@icloud.com>
2022-08-13 17:02:16 +0200

    Initial Commit

---
+++ archive/archiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  archiveApp.swift
+//  archive
+//
+//  Created by Ivan Bottigelli on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct archiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
