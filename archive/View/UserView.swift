dd66ba74647826851c1b4dc6f1efbd328d8f488d
Ivan Bottigelli <testbottyivan@gmail.com>
2022-11-20 23:15:45 +0100

    -> minor improvvisamente to userui

--- archive/View/UserView.swift
+++ archive/View/UserView.swift
@@ -12,39 +12,40 @@ struct UserView: View {
     
     // classi + metodi d'ambiente
     @EnvironmentObject var authenticator: Authenticator
-
-    let lightGreyColor = Color(red: 238.0/255.0, green: 237.0/255.0, blue: 240.0/255.0, opacity: 1.0)
-    let whitesmokeColor = Color(red: 96.0/255.0, green: 96.0/255.0, blue: 95.0/255.0, opacity: 1.0)
-    let darkGreyColor = Color(red: 29.0/255.0, green: 27.0/255.0, blue: 30.0/255.0, opacity: 1.0)
     
-    @Environment(\.colorScheme) var colorScheme
+    let defaults = UserDefaults.standard
     
     var body: some View {
         
+        let username = defaults.string(forKey: "username") ?? "null"
+        
         VStack(alignment: .leading, content: {
             List{
-                HStack{
-                    Spacer()
-                    Image("userNoPhoto")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 150, height: 150)
-                        .cornerRadius(100)
-                    Spacer()
-                }
-                .listRowBackground(colorScheme == .dark ? darkGreyColor : lightGreyColor)
-                Section("USER INFO", content: {
-                    Text("Username: N.D.")
-                    Text("Password: N.D.")
+                VStack(content: {
+                    HStack{
+                        Spacer()
+                        Image("userNoPhoto")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(100)
+                        Spacer()
+                    }
+                    
+                    Text("@\(username)")
+                        .font(.title2.bold())
                 })
+                .listRowBackground(Color.clear)
+                
                 Section("SETTING APP", content: {
+                    Text("Edit credentials")
+                    
                     Button(action: {authenticator.logout()}) {
                         Label("Logout", systemImage: "rectangle.portrait.and.arrow.forward")
                     }
                 })
             }
         })
-        .navigationTitle("Settings")
     }
 }
 
